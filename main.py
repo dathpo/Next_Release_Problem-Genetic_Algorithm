8fbaad4abdc631ab7ca7b8d7a868a66213e69e38
David <davidtpocock@gmail.com>
2018-11-10 13:36:49 +0000

    Implement multiple variable value runs

--- main.py
+++ main.py
@@ -9,17 +9,10 @@ import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 import csv
 import sys
-import numpy as np
 import os.path
 
 
 def main():
-    pwd = os.path.abspath(os.path.dirname(__file__))
-    classic_dir_path = Path("classic-nrp/")
-    realistic_dir_path = Path("realistic-nrp/")
-    classic_nrp = pwd / classic_dir_path / "nrp4.txt"
-    realistic_nrp = pwd / realistic_dir_path / "nrp-m1.txt"
-
     global first_run
     global requirements
     global customers
@@ -27,67 +20,85 @@ def main():
     global weight
     global budget
     global runs
+    x_mo = []
+    y_mo = []
+    x_so = []
+    y_so = []
+    x_rs = []
+    y_rs = []
+    mo_nsga = []
+    so_algorithm = []
 
-    filename = 'realistic_100_09_03'
+    pwd = os.path.abspath(os.path.dirname(__file__))
+    classic_dir_path = Path("classic-nrp/")
+    realistic_dir_path = Path("realistic-nrp/")
+    classic_nrp = pwd / classic_dir_path / "nrp4.txt"
+    realistic_nrp = pwd / realistic_dir_path / "nrp-m1.txt"
+
+    filename = 'classic_100_09_07'
     txt_path = os.path.join(pwd, '{}'.format(filename + '.txt'))
     stdout = sys.stdout
     sys.stdout = open(txt_path, 'w')
 
-    parser = Parser(realistic_nrp)
+    parser = Parser(classic_nrp)
     requirements, customers, req_costs = parser.parse_data()
+
     weight = 0.9
-    budget = 0.3
+    budget = 0.7
     population_size = 100
-    runs = 10000
+    runs = 100
     first_run = True
-
-    mo_problem = Problem(2, 2, 2)
-    mo_problem.types[:] = Binary(len(requirements))
-    mo_problem.directions[0] = Problem.MAXIMIZE
-    mo_problem.directions[1] = Problem.MINIMIZE
-    mo_problem.constraints[:] = "<={}".format(budget)
-    mo_problem.function = multi_objective_nrp
-    mo_nsga = NSGAII(mo_problem, population_size)
-    mo_nsga.run(runs)
-
-    so_problem = Problem(1, 1, 1)
-    so_problem.types[:] = Binary(len(requirements))
-    so_problem.directions[:] = Problem.MAXIMIZE
-    so_problem.constraints[:] = "<={}".format(budget)
-    so_problem.function = single_objective_nrp
-    so_algorithm = GeneticAlgorithm(so_problem, population_size)
-    so_algorithm.run(runs)
-
-    random_vars = run_random(runs, population_size)
+    counter = 0
 
     fig = plt.figure(figsize=(8, 6))
-    x_mo = [solution.objectives[0] for solution in mo_nsga.result]
-    y_mo = [solution.objectives[1] * (-1) for solution in mo_nsga.result]
+
+    # Runs with single parameters values
+    x_mo, y_mo, mo_nsga = mo_run(population_size)
+    x_so, y_so, so_algorithm = so_run(population_size)
+    x_rs, y_rs = rs_run(population_size)
+
+    # Runs with multiple parameters values
+    # for b in range(5, 11):
+    #     budget = b/10
+    #     x_mo, y_mo, mo_nsga = mo_run(population_size)
+    #
+    #     for w in range(1, 10):
+    #         counter += 1
+    #         print("Run #", counter)
+    #         weight = w/10
+    #         x_so, y_so, so_algorithm = so_run(population_size)
+    #
+    #     x_rs, y_rs = rs_run(population_size)
+
     plt.scatter(x_mo, y_mo, 30, color='red', edgecolors='black')
-
-    x_so = [solution.objectives[0] for solution in so_algorithm.result]
-    y_so = [solution.constraints[0] * (-1) for solution in so_algorithm.result]
     plt.scatter(x_so, y_so, 20, marker='s', color='green', edgecolors='black')
-
-    x_rs = [var[0] for var in random_vars]
-    y_rs = [var[1] * (-1) for var in random_vars]
     plt.scatter(x_rs, y_rs, 1, color='c')
 
-    plt.title("Comparison - Realistic data set - pop_size=100, weight=0.9, budget=0.3")
-    # x = x_mo + x_so + x_rs
-    # y = y_mo + y_so + y_rs
-    # plt.xticks(np.arange(min(x), max(x) + 0.01, 0.01))
-    # plt.yticks(np.arange(min(y), max(y) + 0.01, 0.01))
+    plt.title("Comparison - Classic data set - pop_size=100, weight=0.9, budget=0.7")
     plt.xlabel("Score")
-    # plt.xlim([0, 1.1])
     plt.ylabel("-1*Cost")
-    # plt.ylim([0, 1.1])
-    plt.legend(('NSGA−II', 'Single-Objective GA', 'Random search')
-               # , loc='upper left'
-               )
+    plt.legend(('NSGA−II', 'Single-Objective GA', 'Random search'))
 
     graph_path = os.path.join(pwd, '{}'.format(filename + '.pdf'))
     fig.savefig(graph_path, bbox_inches="tight")
+
+    nsga_results = []
+    for nsga_sol in mo_nsga.result:
+        coupled_nsga = (nsga_sol.objectives[0], nsga_sol.objectives[1] * (-1)), sum(nsga_sol.variables[0]), nsga_sol
+        nsga_results.append(coupled_nsga)
+    sorted_nsga = sorted(nsga_results, key=lambda x: x[0])
+    middle_nsga = (len(sorted_nsga) - 1) // 2
+    middle_nsga_sol = sorted_nsga[middle_nsga]
+    print("\nMiddle NSGA Solution:", middle_nsga_sol)
+
+    soga_results = []
+    for soga_sol in so_algorithm.result:
+        coupled_soga = (soga_sol.objectives[0], soga_sol.constraints[0] * (-1)), sum(soga_sol.variables[0]), soga_sol
+        soga_results.append(coupled_soga)
+    sorted_soga = sorted(soga_results, key=lambda x: x[0])
+    middle_soga = (len(sorted_soga) - 1) // 2
+    middle_soga_sol = sorted_soga[middle_soga]
+    print("\nMiddle SO GA Solution:", middle_soga_sol)
 
     csv_path = os.path.join(pwd, '{}'.format(filename + '.csv'))
     with open(csv_path, 'w', newline='') as csv_file:
@@ -99,6 +110,40 @@ def main():
         x_rs.insert(0, 'RS x: Score')
         y_rs.insert(0, 'RS y: -1*Cost')
         writer.writerows((x_mo, y_mo, x_so, y_so, x_rs, y_rs))
+
+
+def mo_run(population_size):
+    mo_problem = Problem(2, 2, 2)
+    mo_problem.types[:] = Binary(len(requirements))
+    mo_problem.directions[0] = Problem.MAXIMIZE
+    mo_problem.directions[1] = Problem.MINIMIZE
+    mo_problem.constraints[:] = "<={}".format(budget)
+    mo_problem.function = multi_objective_nrp
+    mo_nsga = NSGAII(mo_problem, population_size)
+    mo_nsga.run(runs)
+    x_mo = [solution.objectives[0] for solution in mo_nsga.result]
+    y_mo = [solution.objectives[1] * (-1) for solution in mo_nsga.result]
+    return x_mo, y_mo, mo_nsga
+
+
+def so_run(population_size):
+    so_problem = Problem(1, 1, 1)
+    so_problem.types[:] = Binary(len(requirements))
+    so_problem.directions[:] = Problem.MAXIMIZE
+    so_problem.constraints[:] = "<={}".format(budget)
+    so_problem.function = single_objective_nrp
+    so_algorithm = GeneticAlgorithm(so_problem, population_size)
+    so_algorithm.run(runs)
+    x_so = [solution.objectives[0] for solution in so_algorithm.result]
+    y_so = [solution.constraints[0] * (-1) for solution in so_algorithm.result]
+    return x_so, y_so, so_algorithm
+
+
+def rs_run(population_size):
+    random_vars = run_random(runs, population_size)
+    x_rs = [var[0] for var in random_vars]
+    y_rs = [var[1] * (-1) for var in random_vars]
+    return x_rs, y_rs
 
 
 def single_objective_nrp(solution):
